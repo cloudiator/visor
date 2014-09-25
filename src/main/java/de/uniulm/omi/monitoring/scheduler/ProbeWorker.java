b69f56f571742c018ca037bbf084ac1a5941fcb3
Daniel Baur <daniel.baur@uni-ulm.de>
2014-09-25 15:50:26 +0200

    fixed logger import

--- src/main/java/de/uniulm/omi/monitoring/scheduler/ProbeWorker.java
+++ src/main/java/de/uniulm/omi/monitoring/scheduler/ProbeWorker.java
@@ -20,22 +20,22 @@
 
 package de.uniulm.omi.monitoring.scheduler;
 
-import com.sun.media.jfxmedia.logging.Logger;
 import de.uniulm.omi.monitoring.metric.MetricBuilder;
 import de.uniulm.omi.monitoring.probes.api.Probe;
 import de.uniulm.omi.monitoring.probes.impl.MetricNotAvailableException;
 import de.uniulm.omi.monitoring.reporting.api.MetricReportingInterface;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created by daniel on 22.09.14.
  */
-public class ProbeWorker implements Runnable{
+public class ProbeWorker implements Runnable {
 
     protected Probe probe;
     protected MetricReportingInterface metricReportingInterface;
 
-    private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger(ProbeWorker.class);
+    private static final Logger logger = LogManager.getLogger(ProbeWorker.class);
 
     public ProbeWorker(Probe probe, MetricReportingInterface metricReportingInterface) {
         this.probe = probe;
@@ -47,7 +47,7 @@ public class ProbeWorker implements Runnable{
         try {
             this.metricReportingInterface.report(MetricBuilder.getInstance().newMetric(probe.getMetricName(), probe.getMetricValue()));
         } catch (MetricNotAvailableException e) {
-            logger.error(String.format("Could not retrieve metric %s",probe.getMetricName()));
+            logger.error(String.format("Could not retrieve metric %s", probe.getMetricName()));
         }
     }
 }
