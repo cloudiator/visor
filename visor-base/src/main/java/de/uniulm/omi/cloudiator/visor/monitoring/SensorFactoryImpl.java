3d0cb0ca22b646801216166291bd3df0b5e43c55
Joerg Domaschka <joerg.domaschka@uni-ulm.de>
2015-12-18 13:11:53 +0100

    implement extended interface via remote class loaders.

--- visor-base/src/main/java/de/uniulm/omi/cloudiator/visor/monitoring/SensorFactoryImpl.java
+++ visor-base/src/main/java/de/uniulm/omi/cloudiator/visor/monitoring/SensorFactoryImpl.java
@@ -21,19 +21,42 @@ package de.uniulm.omi.cloudiator.visor.monitoring;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 /**
  * Created by daniel on 15.01.15.
  */
 public class SensorFactoryImpl implements SensorFactory {
+	
+	private final Map<URL, ClassLoader> loaders = new WeakHashMap<URL, ClassLoader>();
 
+	@Override
+	public final Sensor fromUriSource(String remoteURI, String className) throws SensorNotFoundException, SensorInitializationException {
+		checkNotNull(remoteURI);
+        checkArgument(!remoteURI.isEmpty());
+		return fromAnySource(remoteURI, remoteURI);
+	}
+	
     @Override
-    public Sensor from(String className) throws SensorNotFoundException, SensorInitializationException {
-        checkNotNull(className);
+    public final Sensor from(String className) throws SensorNotFoundException, SensorInitializationException {
+    	return fromAnySource(null, className); 
+    }
+    
+    protected Sensor fromAnySource(String uri, String className) throws SensorNotFoundException, SensorInitializationException {
+    	checkNotNull(className);
         checkArgument(!className.isEmpty());
-        return this.loadAndInitializeSensor(className);
+        
+        final ClassLoader loader = getClassLoader(uri);
+        checkNotNull(loader);
+		
+        return this.loadAndInitializeSensor(className, loader);
     }
 
-    protected Sensor loadAndInitializeSensor(String className) throws SensorNotFoundException, SensorInitializationException {
+    protected final Sensor loadAndInitializeSensor(String className, ClassLoader loader) throws SensorNotFoundException, SensorInitializationException {
         try {
             Sensor sensor = (Sensor) Class.forName(className).newInstance();
             sensor.init();
@@ -41,6 +64,39 @@ public class SensorFactoryImpl implements SensorFactory {
         } catch (ClassCastException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
             throw new SensorNotFoundException("Could not load sensor with name " + className, e);
         }
+    }
+    
+    private ClassLoader getClassLoader(String remoteUri) throws SensorNotFoundException {
+    	if(null == remoteUri || remoteUri.isEmpty()) {
+    		return SensorFactoryImpl.class.getClassLoader();
+    	}
+    	// FIXME: add checks that this is really a remote uri 
+    	// (e.g. starting with http and not a link to the local
+    	// file system
+    	try {
+    		URL url = new URL(remoteUri);
+    		return getRemoteClassLoader(url);
+    	} catch (MalformedURLException ex) {
+    		throw new SensorNotFoundException("could not create class loader", ex);
+    	}
+    }
+    
+    private ClassLoader getRemoteClassLoader(URL url){
+    	checkNotNull(url);
+    	synchronized(loaders) {
+    		ClassLoader cl = loaders.get(url);
+    		if(cl != null) {
+    			return cl; 
+    		}
+    		return initAndRegisterURLClassLoader(url);
+    	}
+    }
+    
+    /* can only be called with lock on loaders */
+    private URLClassLoader initAndRegisterURLClassLoader(URL url){
+		URLClassLoader urlCl = new URLClassLoader(new URL[]{url}, SensorFactoryImpl.class.getClassLoader());
+		loaders.put(url, urlCl);
+		return urlCl;
     }
 
 }
